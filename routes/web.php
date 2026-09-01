510263ec7f701f0e730aba64205de2b95ea0d854
Fazca <fazca.qais38@smk.belajar.id>
2026-09-01 11:02:18 +0700

    updates routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,5 @@ use App\Http\Controllers\AssetController;
 Route::get('/', function () {
     return redirect('/assets');
 });
-
-// Route CRUD otomatis
+    
 Route::resource('assets', AssetController::class);
